fa166e40320080cf397ae543c4d34249c788ef10
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-02-03 21:36:29 +0430

    fixed error

--- anagram-checker.js
+++ anagram-checker.js
@@ -1,16 +1,16 @@
 module.exports = function (str, str2) {
  
-    let n1 = str1.length;
+    let n1 = str.length;
     let n2 = str2.length;
 
     if (n1 != n2)
         return false;
   
-    str1.sort();
+    str.sort();
     str2.sort()
    
     for (let i = 0; i < n1; i++)
-        if (str1[i] != str2[i])
+        if (str[i] != str2[i])
             return false;
    
     return true;
