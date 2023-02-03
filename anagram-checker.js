035c1382cdd12389fd5777258083aa0aaf61b40b
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-02-03 20:59:31 +0430

    Anagram solution

--- anagram-checker.js
+++ anagram-checker.js
@@ -1,3 +1,11 @@
-module.exports = function (str) {
-  // write your code here
+module.exports = function (str,str2) {
+  let str=str.replace(/[^\w]/g).toLowerCase();
+  let str2=str2.replace(/[^\w]/g).toLowerCase();
+  let str=str.split("").sort().join("");
+  let str2=str2.split("").sort().join("");
+  if(str===str2){
+    return true;
+  }else{
+    return false;
+  }
 }
