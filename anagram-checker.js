8c4f467ba4cf260f59e54ca2c31f9c22d127502f
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-02-03 21:28:44 +0430

    fixed bug

--- anagram-checker.js
+++ anagram-checker.js
@@ -1,6 +1,8 @@
 module.exports = function (str, str2) {
-  let wordOneReplaced = str.replace(/[^\w]/g).toLowerCase();
-  let wordTwoReplaced = str2.replace(/[^\w]/g).toLowerCase();
+  let wordOneReplaced=str.replace(" ","");
+  let wordTwoReplaced=str.replace(" ","");
+   wordOneReplaced = str.replace(/[^\w]/g).toLowerCase();
+   wordTwoReplaced = str2.replace(/[^\w]/g).toLowerCase();
   let stringOne = wordOneReplaced.split("").sort().join("");
   let stringTwo = wordTwoReplaced.split("").sort().join("");
   if (stringOne === stringTwo) {
