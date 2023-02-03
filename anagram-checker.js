60935fc740cbb8c8e6a28ce662dc801ec296a873
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-02-03 21:07:44 +0430

    Fixed error

--- anagram-checker.js
+++ anagram-checker.js
@@ -1,8 +1,8 @@
 module.exports = function (str,str2) {
   let str=str.replace(/[^\w]/g).toLowerCase();
   let str2=str2.replace(/[^\w]/g).toLowerCase();
-  let str=str.split("").sort().join("");
-  let str2=str2.split("").sort().join("");
+   str=str.split("").sort().join("");
+   str2=str2.split("").sort().join("");
   if(str===str2){
     return true;
   }else{
